3b41fd4a2546b7d9b8116d3ce6ffa47c0f2a5c57
Otto von Wesendonk <ottovonwesendonk@gmail.com>
2022-12-11 16:08:24 +0100

    Added initial text.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,49 @@
 export default function Home() {
   return (
-    <div className="">
-        Hello, World. Ottomation coming soon.
+    <div className="container mx-auto p-10">
+      <div className="mb-10">
+        <h2 className="text-2xl my-5">Need help? Want to collaborate?</h2>
+        <p className="prose">
+          As tech advisor & enthusiast I can help you with <strong>tech strategy, concepts, tech leadership & coaching and hands-ons development</strong> (I don&apos;t like Loosing touch). Focus lies on helping startups getting from 0 to 1 and grown ups from 1 to 10. And I love architecting distributed & scaling systems.
+        </p>
+
+        <h1 className="text-4xl my-5">Say <a className="underline font-bold text-green-800 hover:text-blue-500" href="mailto:hi@ottomation.tech">hi@ottomation.tech</a></h1>
+        <p>
+          Let me <i>ottomate</i> your world 🙂
+        </p>
+      </div>
+
+      <ul className="mb-10">
+        <li><a href="https://www.linkedin.com/in/ottovonwesendonk/" target={'_blank'} className="font-bold text-blue-500 hover:text-blue-800" rel="noreferrer">LinkedIn: Otto von Wesendonk</a></li>
+        <li><a href="https://twitter.com/ottomotton" target={'_blank'} className="font-bold text-blue-500 hover:text-blue-800" rel="noreferrer">Twitter: @ottomotton</a></li>
+        <li><a href="https://mstdn.social/@ottomotton" target={'_blank'} className="font-bold text-blue-500 hover:text-blue-800" rel="noreferrer">Mastodon: @ottomotton@mstdn.social
+        </a></li>
+        <li><a href="https://ottovw.com" target={'_blank'} className="font-bold text-blue-500 hover:text-blue-800" rel="noreferrer">Blog: https://ottovw.com</a></li>
+      </ul>
+
+      <div className="prose mb-10">
+        <h3>My Profile</h3>
+        <p>
+          Builder, Thinker & Communicator<br />
+          with Passion for Innovation and Systems theory.
+        </p>
+        <p>
+          As a builder my typical roles involve tech leadership, software architecture and fullstack. My love for designing solutions and technical excellence is combined with a strong bias for action. I have a track record of bringing ideas from 1 to 0 and scaling existing solutions to a global scale.
+        </p><p>
+          As a thinker I&apos; m your sparring partner. Next to technical perspective I also offer strong product and business sense. My experience ranges from many early stage and later stage projects I love to help conceptualize innovative digital solutions and evaluate business ideas and models.
+        </p><p>
+          As a communicator I build bridges and enable understanding between different groups: Collaboration between startups and big organizations. Between tech pros themselves or the evergreen triangle: between business, product and tech.
+        </p><p>
+          I thrive in high-paced and exploratory environments. They say I&apos;m an inspiring leader and I love helping people to grow as much as growing myself.
+        </p>
+      </div>
+      <div>
+        Otto von Wesendonk<br />
+        Ottomation GmbH <br />
+        Berlin, Germany <br />
+      </div>
     </div>
+
+
   )
 }
